2dd75308d6b1e047b1f9aba594e042f35aa68ceb
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-09-08 10:51:07 +0900

    Image Loading Optimization: Add lazy loading to image elements and set placeholders

--- EmotionObject.js
+++ EmotionObject.js
@@ -26,7 +26,11 @@ class EmotionObject {
               <p>${wordObj.definition}</p>
             </div>
             <figure class="col-5">
-              <img src="${wordObj.pictureUrl}" alt="${wordObj.word}を表現する画像" loading="lazy">
+              <img class="lazy-image"
+                   data-src="${wordObj.pictureUrl}"
+                   alt="${wordObj.word}のイメージ画像"
+                   loading="lazy"
+                   src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100'%3E%3Crect width='100' height='100' fill='%23f0f0f0'/%3E%3Ctext x='50%25' y='50%25' text-anchor='middle' dy='.3em' fill='%23999'%3E読み込み中...%3C/text%3E%3C/svg%3E">
             </figure>
           </article>
         `;
